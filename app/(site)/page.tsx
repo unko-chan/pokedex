93d975344240ba9c0680b7f3584817c303b74146
Kevin Liang <kevinliang0303@gmail.com>
2023-05-13 18:01:33 -0700

    Set up main page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,35 @@
-export default function Home() {
-  return <div></div>;
+'use client';
+import React, { useEffect, useState } from 'react';
+import { getPokemonList } from './utils/pokeapi';
+import PokemonCard from './components/PokemonCard';
+
+interface Pokemon {
+  name: string;
+  url: string;
 }
+
+const Home: React.FC = () => {
+  const [offset, setOffset] = useState(0);
+  const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+  const limit = 10
+
+  useEffect(() => {
+    getPokemonList(offset, limit).then(response => {
+      console.log(response);
+      setPokemonList(response.results);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Pokédex</h1>
+      <div className="pokemon-list">
+        {pokemonList.map(pokemon => (
+          <PokemonCard key={pokemon.name} pokemon={pokemon} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
