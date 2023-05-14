7e3583df3685df73b2ec95a6191d5de0cee3f6f0
Kevin Liang <kevinliang0303@gmail.com>
2023-05-13 21:49:03 -0700

    Implement filtering

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,39 +1,109 @@
-'use client';
-import React, { useEffect, useState } from 'react';
-import { getPokemonList } from './utils/pokeapi';
-import PokemonCard from './components/PokemonCard';
-import Pagination from './components/Pagination';
+"use client";
+import React, { use, useEffect, useState } from "react";
+import {
+  getPokemonList,
+  getAllPokemon,
+  getPokemonByType,
+  getTypes,
+} from "./utils/pokeapi";
+import PokemonCard from "./components/PokemonCard";
+import Pagination from "./components/Pagination";
+import Filter from "./components/Filter";
 
 interface Pokemon {
   name: string;
   url: string;
+  types: [{ name: string }];
+}
+
+interface Types {
+  name: string;
+  url: string;
+}
+
+interface PokemonItem {
+  pokemon: {
+    name: string;
+  };
 }
 
 const POKEMON_PER_PAGE = 10;
 
 const Home: React.FC = () => {
   const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+  const [currentPokemonList, setCurrentPokemonList] = useState<Pokemon[]>([]);
+  const [filteredPokemonList, setFilteredPokemonList] = useState<Pokemon[]>([]);
+  const [types, setTypes] = useState<Types[]>([]);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(5);
   const [count, setCount] = useState(0);
-  const offset = (page - 1) * POKEMON_PER_PAGE
+  const [selectedTypes, setSelectedTypes] = useState<string[]>([]);
+  const offset = (page - 1) * POKEMON_PER_PAGE;
 
   useEffect(() => {
-    getPokemonList(offset, POKEMON_PER_PAGE).then(response => {
-      console.log(response);
-      setCount(response.count);
+    getAllPokemon().then((response) => {
       setPokemonList(response.results);
+      setFilteredPokemonList(response.results);
+      setCount(response.count);
       setTotalPages(Math.ceil(response.count / POKEMON_PER_PAGE));
     });
-  }, [page]);
+
+    getTypes().then((response) => {
+      setTypes(response.results);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (selectedTypes.length > 0) {
+      const promises = selectedTypes.map((type) => getPokemonByType(type));
+
+      Promise.all(promises).then((typeResponses) => {
+        const filteredNamesSet = new Set();
+
+        typeResponses.forEach((response) => {
+          response.forEach((pokemonItem: PokemonItem) =>
+            filteredNamesSet.add(pokemonItem.pokemon.name)
+          );
+        });
+
+        const filteredNames = Array.from(filteredNamesSet);
+
+        const filteredPokemon = pokemonList.filter((pokemon) =>
+          filteredNames.includes(pokemon.name)
+        );
+        setFilteredPokemonList(filteredPokemon);
+      });
+    } else {
+      setFilteredPokemonList(pokemonList);
+    }
+  }, [selectedTypes]);
+
+  useEffect(() => {
+    const pokemonOnPage = filteredPokemonList.slice(offset, offset + POKEMON_PER_PAGE);
+    setCurrentPokemonList(pokemonOnPage);
+  }, [page, offset, filteredPokemonList]);
 
   return (
     <div>
       <h1>Pokédex</h1>
-      <div>Showing {offset + 1} - {offset + pokemonList.length} of {count}</div>
-      <Pagination page={page} totalPages={totalPages} handlePageChange={setPage} />
+      <Filter
+        types={types}
+        selectedTypes={selectedTypes}
+        setSelectedTypes={setSelectedTypes}
+      />
+      <div>
+        Showing {offset + 1} - {offset + currentPokemonList.length} of {count}
+      </div>
+      <div>
+        Page {page} of {totalPages}
+      </div>
+      <Pagination
+        page={page}
+        totalPages={totalPages}
+        handlePageChange={setPage}
+      />
       <div className="pokemon-list">
-        {pokemonList.map(pokemon => (
+        {currentPokemonList.map((pokemon) => (
           <PokemonCard key={pokemon.name} pokemon={pokemon} />
         ))}
       </div>
